d0cda26c8e8ea6a8617bcacabb10a790beca6004
andela-nmartin <godmetweenciati@gmail.com>
2015-12-03 15:39:47 +0300

    Hound ci issues

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ http.listen(config.port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on port: " + config.port);
+    console.log('Listening on port: ' + config.port);
   }
 });
